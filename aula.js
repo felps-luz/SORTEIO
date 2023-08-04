261650f183caf60c88d16eb86fc79a3230696d21
felps-luz <felipeluz143@gmail.com>
2023-08-04 17:00:00 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,15 @@
+//qualquer numero quebrado
+console.log(Math.random());
+console.log(Math.random() * 20);
+console.log(Math.random(3 - 7 + 1)+3);
+
+//arredondar para numero inteiro mais proximo
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+//arredondar para baixo
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
+
+//arredondar para cima
+console.log(Math.ceil(3.1))
